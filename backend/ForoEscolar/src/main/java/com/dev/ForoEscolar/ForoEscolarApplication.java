af2a2b46592b3f31ef96fccbed426cded7f81192
Jose Angel Suarez <angelsuarez2612@gmail.com>
2024-08-23 21:56:17 -0300

    Creando Proyecto y subiendo al repo

---
+++ backend/ForoEscolar/src/main/java/com/dev/ForoEscolar/ForoEscolarApplication.java
@@ -0,0 +1,13 @@
+package com.dev.ForoEscolar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForoEscolarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForoEscolarApplication.class, args);
+	}
+
+}
---
+++ backend/ForoEscolar/src/test/java/com/dev/ForoEscolar/ForoEscolarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.ForoEscolar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForoEscolarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
