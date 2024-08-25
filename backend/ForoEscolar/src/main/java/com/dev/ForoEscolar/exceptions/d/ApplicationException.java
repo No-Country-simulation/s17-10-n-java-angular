98ad7962d5095f59923d88c155d40a534f5d932f
Samir López <lopezs.dev@gmail.com>
2024-08-25 16:17:31 -0600

    Se agrega un manejador de exepciones bÂbásicas y uno global

---
+++ backend/ForoEscolar/src/main/java/com/dev/ForoEscolar/exceptions/d/ApplicationException.java
@@ -0,0 +1,22 @@
+package com.dev.ForoEscolar.exceptions.d;
+
+public class ApplicationException extends RuntimeException{
+
+    private String campo;
+
+    public ApplicationException(String mensaje) {
+        super(mensaje);
+    }
+
+    public ApplicationException(String campo, String mensaje) {
+        super(mensaje);
+        this.campo = campo;
+    }
+
+    public String getCampo() {
+        return campo;
+    }
+
+    public void setCampo(String campo) {
+        this.campo = campo;
+    }}
