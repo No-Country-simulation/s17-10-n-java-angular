49a4d20c3b89e1f3ef160a95bc78e37e3fe827c5
Nicolás Lemos <nico.lemos77@gmail.com>
2024-08-31 19:49:45 -0300

    agregando sidebar

--- frontend/forEdu/src/app/app.component.ts
+++ frontend/forEdu/src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {SidebarComponent} from "./components/sidebar/sidebar.component";
+import {AdmiDashboardComponent} from "./Admin/admin-layout/admi-dashboard/admi-dashboard.component";
+import {PanelMenuModule} from "primeng/panelmenu";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SidebarComponent, AdmiDashboardComponent, PanelMenuModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- frontend/forEdu/src/app/components/sidebar/sidebar.component.ts
+++ frontend/forEdu/src/app/components/sidebar/sidebar.component.ts
@@ -1,12 +1,61 @@
 import { Component } from '@angular/core';
+import { PanelMenuModule } from 'primeng/panelmenu';
+import {MenuItem} from "primeng/api";
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [PanelMenuModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
-export class SidebarComponent {
 
+
+export class SidebarComponent {
+  items: MenuItem[];
+
+  constructor() {
+    this.items = [
+      {
+        label: 'Inicio',
+        icon: 'pi pi-fw pi-home',
+      },
+      {
+        label: 'Grados',
+        icon: 'pi pi-fw pi-book',
+      },
+      {
+        label: 'Calendarios',
+        icon: 'pi pi-fw pi-calendar',
+      },
+      {
+        label: 'Mensajes',
+        icon: 'pi pi-fw pi-inbox',
+      }
+      ,
+      {
+        label: 'Usuarios',
+        icon: 'pi pi-fw pi-users',
+      }
+      ,
+      {
+        label: 'Boletines',
+        icon: 'pi pi-fw pi-graduation-cap',
+      },
+      {
+        label: 'Herramientas',
+        icon: 'pi pi-fw pi-cog',
+        items: [
+          {
+            label: 'Setting',
+            icon: 'pi pi-fw pi-cog',
+          },
+          {
+            label: 'Soporte',
+            icon: 'pi pi-fw pi-info-circle',
+          }
+        ]
+      }
+    ];
+  }
 }
