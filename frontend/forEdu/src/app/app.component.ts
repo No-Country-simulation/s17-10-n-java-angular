93a0b7bb580376af70b2b15a0d3a9a9b3c703a56
HugoCatalan <hugocatalan2@gmail.com>
2024-09-02 19:48:27 -0300

    registro y login

--- frontend/forEdu/src/app/app.component.ts
+++ frontend/forEdu/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from "./components/login/login.component";
+import { RegisterComponent } from "./components/register/register.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoginComponent, RegisterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
