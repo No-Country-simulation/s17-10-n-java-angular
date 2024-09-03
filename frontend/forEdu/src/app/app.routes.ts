ab4606b3fe551df511ea4386498aad5f8f6a394c
Zail <rekcahet@gmail.com>
2024-09-02 21:54:02 -0500

    adfasd

--- frontend/forEdu/src/app/app.routes.ts
+++ frontend/forEdu/src/app/app.routes.ts
@@ -1,19 +1,19 @@
 import { Routes } from '@angular/router';
 import { ADMIN_ROUTES } from './Admin/admin.routes';
 import { InstitutionalUserComponent } from './components/institutional-user/institutional-user.component';
-import {SidebarComponent} from "./components/sidebar/sidebar.component";
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 export const routes: Routes = [
-
-    {
-        path: '',
-        children: ADMIN_ROUTES
-      },
-      {
-        path: 'table', component: InstitutionalUserComponent
-      },
   {
-    path: 'sidemenu', component: SidebarComponent
-  }
-
+    path: '',
+    children: ADMIN_ROUTES,
+  },
+  {
+    path: 'table',
+    component: InstitutionalUserComponent,
+  },
+  {
+    path: 'sidemenu',
+    component: SidebarComponent,
+  },
 ];
