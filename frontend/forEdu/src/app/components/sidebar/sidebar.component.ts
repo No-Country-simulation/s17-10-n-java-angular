800c7a7ec80f6895ca5cf43e8ec939a0d335b179
Nicolás Lemos <nico.lemos77@gmail.com>
2024-09-02 23:04:00 -0300

    modificando el style y el angular.json

--- frontend/forEdu/src/app/components/sidebar/sidebar.component.ts
+++ frontend/forEdu/src/app/components/sidebar/sidebar.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ViewEncapsulation } from '@angular/core';
 import { MenuItem } from 'primeng/api';
 import { PanelMenuModule } from 'primeng/panelmenu';
-import { StyleClassModule } from 'primeng/styleclass';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
   imports: [PanelMenuModule],
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.css'
+  encapsulation: ViewEncapsulation.None,
+  styleUrls: ['./sidebar.component.css']
 })
+
 
 
 export class SidebarComponent implements OnInit {
