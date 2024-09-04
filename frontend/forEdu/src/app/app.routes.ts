bb785356bb03b1f6fa39ea798b672ff1a4889ea5
Zail <rekcahet@gmail.com>
2024-09-03 22:07:10 -0500

    add

--- frontend/forEdu/src/app/app.routes.ts
+++ frontend/forEdu/src/app/app.routes.ts
@@ -2,11 +2,22 @@ import { Routes } from '@angular/router';
 import { ADMIN_ROUTES } from './Admin/admin.routes';
 import { InstitutionalUserComponent } from './components/institutional-user/institutional-user.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { GradeComponent } from './Admin/admin-grade/grade/grade.component';
 
 export const routes: Routes = [
   {
     path: '',
-    children: ADMIN_ROUTES,
+    children: [
+      ...ADMIN_ROUTES, // Aquí expandes las rutas que tienes en ADMIN_ROUTES
+      {
+        path: 'grade',
+        component: GradeComponent,
+      },
+      {
+        path: 'calendar',
+        component: GradeComponent,
+      },
+    ],
   },
   {
     path: 'table',
