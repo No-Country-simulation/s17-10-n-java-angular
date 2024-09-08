9403b98533f4cb79c9213b2822843560c73b505c
Gonzalo Villegas <gvillegas1276@hotmail.com>
2024-09-08 16:57:55 -0500

    fix: se corrige applications.properties

--- backend/ForoEscolar/target/generated-sources/annotations/com/dev/ForoEscolar/mapper/boletin/BoletinMapperImpl.java
+++ backend/ForoEscolar/target/generated-sources/annotations/com/dev/ForoEscolar/mapper/boletin/BoletinMapperImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-09-08T16:49:13-0500",
+    date = "2024-09-08T16:56:41-0500",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 21.0.2 (Oracle Corporation)"
 )
 @Component
