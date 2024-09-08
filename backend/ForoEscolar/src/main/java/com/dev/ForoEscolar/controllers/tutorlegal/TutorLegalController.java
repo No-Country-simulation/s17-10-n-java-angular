9c68d9e6934670930d65f3f5357192a1940505d4
Gonzalo Villegas <gvillegas1276@hotmail.com>
2024-09-08 09:36:53 -0500

    feat: se agregan metodos de tutor by samir

--- backend/ForoEscolar/src/main/java/com/dev/ForoEscolar/controllers/tutorlegal/TutorLegalController.java
+++ backend/ForoEscolar/src/main/java/com/dev/ForoEscolar/controllers/tutorlegal/TutorLegalController.java
@@ -3,6 +3,7 @@ package com.dev.ForoEscolar.controllers.tutorlegal;
 import com.dev.ForoEscolar.dtos.ApiResponseDto;
 import com.dev.ForoEscolar.dtos.tutorlegal.TutorLegalRequestDTO;
 import com.dev.ForoEscolar.dtos.tutorlegal.TutorLegalResponseDTO;
+import com.dev.ForoEscolar.exceptions.ApplicationException;
 import com.dev.ForoEscolar.services.TutorLegalService;
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
@@ -10,10 +11,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("api/tutorlegal")
@@ -26,6 +26,25 @@ public class TutorLegalController {
         this.tutorLegalService = tutorLegalService;
     }
 
+    @GetMapping("/getAll")
+    @Operation(summary = "se obtiene toda la lista de tutores legales")
+    public ResponseEntity<Iterable<TutorLegalResponseDTO>> getAll() {
+        try{
+            Iterable<TutorLegalResponseDTO> list = tutorLegalService.findAll();
+            return new ResponseEntity<>(list, HttpStatus.OK);
+        }catch (ApplicationException e){
+            throw new ApplicationException(" Ocurrió un error al retornar la lista " +e.getMessage());
+        }
+    }
+
+    @GetMapping("/{id}")
+    @Operation(summary = "se obtiene un tutor legal por su id")
+    public ResponseEntity<TutorLegalResponseDTO> getById(@PathVariable Long id) {
+        Optional<TutorLegalResponseDTO> tutor = tutorLegalService.findById(id);
+        return tutor.map(tutorLegalResponseDTO -> new ResponseEntity<>(tutorLegalResponseDTO, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
     @PostMapping(value = "/register", produces = MediaType.APPLICATION_JSON_VALUE)
     @Operation(summary = "Registra un nuevo tutor legal")
     public ResponseEntity<ApiResponseDto<TutorLegalResponseDTO>> registerTutorLegal(@RequestBody @Valid TutorLegalRequestDTO tutorLegalRequestDTO) {
@@ -36,5 +55,19 @@ public class TutorLegalController {
         }
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(new ApiResponseDto<>(true, "Tutor legal registrado", tutorLegal));
+    }
+
+    @PostMapping("/update")
+    @Operation(summary = "actualiza un tutor legal específico")
+    public ResponseEntity<TutorLegalResponseDTO> update(TutorLegalRequestDTO tutorLegalRequestDTO) {
+        TutorLegalResponseDTO Tutor = tutorLegalService.update(tutorLegalRequestDTO);
+        return new ResponseEntity<>(Tutor, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    @Operation(summary = "elimina un tutor legal dando como parametro el id correspondiente")
+    public ResponseEntity<TutorLegalResponseDTO> delete(@PathVariable Long id) {
+        tutorLegalService.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
