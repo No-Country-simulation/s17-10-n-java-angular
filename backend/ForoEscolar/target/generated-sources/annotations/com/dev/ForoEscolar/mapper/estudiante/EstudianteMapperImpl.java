1e2e12e20219e51e9d21130cd58cac04f66b0feb
Gonzalo Villegas <gvillegas1276@hotmail.com>
2024-09-08 17:37:56 -0500

    feat: deploy v0.1

--- backend/ForoEscolar/target/generated-sources/annotations/com/dev/ForoEscolar/mapper/estudiante/EstudianteMapperImpl.java
+++ backend/ForoEscolar/target/generated-sources/annotations/com/dev/ForoEscolar/mapper/estudiante/EstudianteMapperImpl.java
@@ -15,7 +15,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-09-08T16:56:41-0500",
+    date = "2024-09-08T17:36:51-0500",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 21.0.2 (Oracle Corporation)"
 )
 @Component
