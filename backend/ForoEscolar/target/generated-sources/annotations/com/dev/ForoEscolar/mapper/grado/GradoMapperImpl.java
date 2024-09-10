49492006041e6cc204cc8347a3b641090b7572d8
Gonzalo Villegas <gvillegas1276@hotmail.com>
2024-09-10 11:59:05 -0500

    feat: actualizo rama local angel con rama remota angel

--- backend/ForoEscolar/target/generated-sources/annotations/com/dev/ForoEscolar/mapper/grado/GradoMapperImpl.java
+++ backend/ForoEscolar/target/generated-sources/annotations/com/dev/ForoEscolar/mapper/grado/GradoMapperImpl.java
@@ -7,8 +7,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-09-10T04:22:45-0300",
-    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 22.0.2 (Oracle Corporation)"
+    date = "2024-09-10T11:56:14-0500",
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 21.0.2 (Oracle Corporation)"
 )
 @Component
 public class GradoMapperImpl extends GradoMapper {
