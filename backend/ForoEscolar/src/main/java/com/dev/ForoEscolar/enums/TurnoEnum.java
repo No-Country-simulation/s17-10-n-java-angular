38c7d7416cbf49162cd2b0ddcdfc66065b8ee136
Gonzalo Villegas <gvillegas1276@hotmail.com>
2024-09-14 10:21:16 -0500

    fix: se realiza el cambio en TurnoEnum.java

--- backend/ForoEscolar/src/main/java/com/dev/ForoEscolar/enums/TurnoEnum.java
+++ backend/ForoEscolar/src/main/java/com/dev/ForoEscolar/enums/TurnoEnum.java
@@ -1,7 +1,7 @@
 package com.dev.ForoEscolar.enums;
 
 public enum TurnoEnum {
-    MANANA,
+    MANAÑA,
     TARDE,
     NOCHE;
 
--- backend/ForoEscolar/target/generated-sources/annotations/com/dev/ForoEscolar/mapper/profesor/ProfesorMapperImpl.java
+++ backend/ForoEscolar/target/generated-sources/annotations/com/dev/ForoEscolar/mapper/profesor/ProfesorMapperImpl.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-09-10T12:05:51-0500",
+    date = "2024-09-11T17:38:34-0500",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 21.0.2 (Oracle Corporation)"
 )
 @Component
